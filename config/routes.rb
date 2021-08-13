ccd7b5d7b0b254f02c6e195410980a859d5c867b
NorifumiOgawa <n@ogaworks.com>
2021-08-13 11:05:28 +0900

    本一覧のルーティングを変更してみる

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   }
   get '/users', to:'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :books
+  resources :books, :except => :index
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
