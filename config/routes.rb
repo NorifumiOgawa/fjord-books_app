72747a472329677c7e59b3b85dc1ba376faca958
NorifumiOgawa <n@ogaworks.com>
2021-08-13 10:39:03 +0900

    レビュー後の修正１回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/' => redirect('/books')
+  root to: 'books#index'
   devise_for :users, :controllers => {
     :registrations => "users/registrations"
   }
