37d1e25b42041741bae5698fa758b7266a55dd5d
NorifumiOgawa <n@ogaworks.com>
2021-08-17 11:51:25 +0900

    モデルの関連付け

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followee, class_name: 'User'
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
