15810e6fe8611506f4fab36eeca007a367f09fd1
NorifumiOgawa <n@ogaworks.com>
2021-08-24 20:20:02 +0900

    エラーをrescueで捕まえる

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,10 @@
 class RelationshipsController < ApplicationController
   def create
     @other_user = User.find(params[:user_id])
-    if current_user.follow(@other_user)
-      redirect_to user_path(@other_user)
-    else
-      redirect_to user_path(@other_user), alert: t('errors.template.header.one', model: Relationship.model_name.human)
-    end
+    current_user.follow(@other_user)
+    redirect_to user_path(@other_user)
+  rescue StandardError => e
+    redirect_to user_path(@other_user), alert: e
   end
 
   def destroy
