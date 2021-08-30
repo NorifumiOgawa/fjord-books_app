900d71d4542da63e659e6610f85585538842c979
NorifumiOgawa <n@ogaworks.com>
2021-08-30 10:23:18 +0900

    indexの不整合を解消する

--- db/migrate/20210816020717_create_relationships.rb
+++ db/migrate/20210816020717_create_relationships.rb
@@ -9,6 +9,5 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
       t.timestamps
     end
     add_index :relationships, %i[follower_id followed_id], unique: true
-    add_index :relationships, :followed_id
   end
 end
